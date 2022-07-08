96886f8162b10642c02fee1bdd28a521f30acbc2
RobiTheGit <94720060+RobiTheGit@users.noreply.github.com>
2022-07-08 07:58:20 -0600

    adds the directories

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+import time
+print('making directories')
+os.mkdir('Downloads')
+print('Downloads made')
+time.sleep(1)
+os.mkdir('Documents')
+print('Documents made')
+time.sleep(1)
+os.mkdir('Pictures')
+print('Pictures made')
+time.sleep(1)
+os.mkdir('Music')
+print('Music made')
+time.sleep(1)
+os.mkdir('Videos')
+print('Videos made')
+print('All Done with directories')
+
